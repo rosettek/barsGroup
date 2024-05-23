f09b2ad54cb4a3a262453b6907267b188223cf6d
airat <airat.hb@mail.ru>
2024-05-23 22:00:02 +0300

    Book -> Task

--- TaskManager.Domain/Abstractions/ITasksService.cs
+++ TaskManager.Domain/Abstractions/ITasksService.cs
@@ -1,16 +1,10 @@
-﻿
-
-
-
-
-
-namespace TaskManager.Application.Services
+﻿namespace TaskManager.Application.Services
 {
     public interface ITasksService
     {
-        Task<Guid> CreateBook(Domain.Models.Task task);
-        Task<Guid> DeleteBook(Guid id);
+        Task<Guid> CreateTask(Domain.Models.Task task);
+        Task<Guid> DeleteTask(Guid id);
         Task<List<Domain.Models.Task>> GetAllTasks();
-        Task<Guid> UpdateBook(Guid id, string title, string tittle, string description, DateTime deadline, bool taskStatus);
+        Task<Guid> UpdateTask(Guid id, string title, string tittle, string description, DateTime deadline, bool taskStatus);
     }
 }
