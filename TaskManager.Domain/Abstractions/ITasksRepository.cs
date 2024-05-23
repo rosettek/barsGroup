3520d0e658d79dc7b543dbf7c33cdcd31609f46f
rosettek <rosettek@student.21-school.ru>
2024-05-23 17:42:12 +0300

    add isDeleted

---
+++ TaskManager.Domain/Abstractions/ITasksRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManager.DataAccess.Repositories
+{
+    public interface ITasksRepository
+    {
+        Task<Guid> Create(Domain.Models.Task task);
+        Task<Guid> Delete(Guid id);
+        Task<List<Domain.Models.Task>> Get();
+        Task<Guid> Update(Guid id, string title, string tittle, string description, DateTime deadline, bool taskStatus);
+    }
+}
--- TaskManager.Domain/Models/Task.cs
+++ TaskManager.Domain/Models/Task.cs
@@ -1,12 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TaskManager.Domain.Models
+﻿namespace TaskManager.Domain.Models
 {
-    internal class Task
+    public class Task
     {
+        public const int MAX_TITLE_LENGHT = 250;
+        public const int MAX_DESCRIPTION_LENGHT = 500;
+
+        private Task(Guid id, string title, string description,
+            DateTime deadline, DateTime createDate, bool taskStatus, bool isDeleted)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            Deadline = deadline;
+            CreateDate = createDate;
+            TaskStatus = taskStatus;
+            IsDeleted = isDeleted;
+        }
+
+        public Guid Id { get; }
+
+        public string Title { get; } = string.Empty;
+
+        public string Description { get; } = string.Empty;
+
+        public DateTime Deadline { get; } = DateTime.UtcNow;
+
+        public DateTime CreateDate { get; } = DateTime.UtcNow;
+
+        public bool TaskStatus { get; } = true;
+
+        public bool IsDeleted { get; } = false;
+
+
+        public static (Task Task, string Error) Create(Guid id, string title, string description,
+            DateTime deadline, DateTime createDate, bool taskStatus, bool isDeleted)
+        {
+            string errorString = string.Empty;
+
+            if (string.IsNullOrEmpty(title) || title.Length > MAX_TITLE_LENGHT)
+                errorString = "Error in task tittle";
+
+            if (string.IsNullOrEmpty(description) || description.Length > MAX_DESCRIPTION_LENGHT)
+                errorString = "Error in task description";
+
+            var task = new Task(id, title, description, deadline, createDate, taskStatus, isDeleted);
+
+            return (task, errorString);
+        }
     }
 }
