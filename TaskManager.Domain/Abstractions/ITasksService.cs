1fcf3e1eae39a48c1b8da4c621b91c289600b890
rosettek <rosettek@student.21-school.ru>
2024-05-23 18:23:42 +0300

    add services

---
+++ TaskManager.Domain/Abstractions/ITasksService.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+
+
+
+namespace TaskManager.Application.Services
+{
+    public interface ITasksService
+    {
+        Task<Guid> CreateBook(Domain.Models.Task task);
+        Task<Guid> DeleteBook(Guid id);
+        Task<List<Domain.Models.Task>> GetAllTasks();
+        Task<Guid> UpdateBook(Guid id, string title, string tittle, string description, DateTime deadline, bool taskStatus);
+    }
+}
