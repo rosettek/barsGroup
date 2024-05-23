a114985af2d07ca684661c72a4db126c04190c28
rosettek <rosettek@student.21-school.ru>
2024-05-23 18:22:31 +0300

    add services

---
+++ TaskManager.Application/Services/TasksService.cs
@@ -0,0 +1,36 @@
+﻿using TaskManager.DataAccess.Repositories;
+
+namespace TaskManager.Application.Services
+{
+    public class TasksService : ITasksService
+    {
+        private readonly ITasksRepository _taskRepositories;
+
+        public TasksService(ITasksRepository taskRepositories)
+        {
+            _taskRepositories = taskRepositories;
+        }
+
+        public async Task<List<Domain.Models.Task>> GetAllTasks()
+        {
+            return await _taskRepositories.Get();
+        }
+
+        public async Task<Guid> CreateBook(Domain.Models.Task task)
+        {
+            return await _taskRepositories.Create(task);
+        }
+
+        public async Task<Guid> UpdateBook(Guid id, string title, string tittle,
+                                           string description, DateTime deadline, bool taskStatus)
+        {
+            return await _taskRepositories.Update(id, title, tittle,
+                                                  description, deadline, taskStatus);
+        }
+
+        public async Task<Guid> DeleteBook(Guid id)
+        {
+            return await _taskRepositories.Delete(id);
+        }
+    }
+}
