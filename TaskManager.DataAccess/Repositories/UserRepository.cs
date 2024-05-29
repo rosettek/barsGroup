3a424b5e6c8f95960760bf140dfa7f7569e2b4c5
airat <airat.hb@mail.ru>
2024-05-30 01:52:22 +0300

    new repository for user

---
+++ TaskManager.DataAccess/Repositories/UserRepository.cs
@@ -0,0 +1,57 @@
+﻿//using Microsoft.EntityFrameworkCore;
+//using TaskManager.DataAccess.Entities;
+//using User.Domain.Models;
+
+//namespace TaskManager.DataAccess.Repositories
+//{
+//    public class UserRepository
+//    {
+//        private readonly TaskManagerDbContext _context;
+
+//        public UserRepository(TaskManagerDbContext context)
+//        {
+//            _context = context;
+//        }
+
+//        public async Task<List<User>> Get()
+//        {
+//            var userEntity = await _context.Users
+//                .AsNoTracking()
+//                .ToListAsync();
+
+//            var users = userEntity.Select(b => User.Create(
+//                b.User_id,
+//                b.Name,
+//                b.Email,
+//                b.PasswordHash
+//                )).ToList();
+
+//            return users;
+//        }
+
+//        public async Task<Guid> Add(User user)
+//        {
+//            var userEntity = new UserEntity
+//            {
+//                User_id = user.User_id,
+//                Name = user.Name,
+//                Email = user.Email,
+//                PasswordHash = user.PasswordHash,
+//            };
+
+//            await _context.Users.AddAsync(userEntity);
+//            await _context.SaveChangesAsync();
+
+//            return userEntity.User_id;
+//        }
+
+//        public async Task<User> GetByEmail(string email)
+//        {
+//            var userEntity = await _context.Users
+//                .AsNoTracking()
+//                .FirstOrDefaultAsync(u => u.Email == email) ?? throw new Exception();
+
+//            return _mapper.Map<User>(userEntity);
+//        }
+//    }
+//}
