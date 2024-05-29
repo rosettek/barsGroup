97beef31c33dda74cf591194896372fc46ff0778
airat <airat.hb@mail.ru>
2024-05-30 01:52:03 +0300

    new entity user

---
+++ TaskManager.DataAccess/Entities/UserEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskManager.DataAccess.Entities
+{
+    public class UserEntity
+    {
+        public Guid User_id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Email { get;  set; } = string.Empty;
+
+        public string PasswordHash { get;  set; } = string.Empty;
+    }
+}
