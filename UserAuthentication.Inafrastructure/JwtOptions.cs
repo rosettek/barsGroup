213eba8a550dd3d3273ca7e39bfd28d7eb3f8729
airat <airat.hb@mail.ru>
2024-05-30 21:21:40 +0300

    add Jwt

---
+++ UserAuthentication.Inafrastructure/JwtOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace UserAuthentication.Inafrastructure
+{
+    public class JwtOptions
+    {
+        public string SecretKey { get; set; } = String.Empty;
+
+        public int ExpitesHours { get; set; }
+    }
+}
---
+++ UserAuthentication.Inafrastructure/JwtProvider.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using UserAuthentication.Domain.Abstaraction;
+using UserAuthentication.Domain.Models;
+
+namespace UserAuthentication.Inafrastructure
+{
+    public class JwtProvider(IOptions<JwtOptions> options) : IJwtProvider
+    {
+        private readonly JwtOptions _options = options.Value;
+
+        public string GenerateToken(User user)
+        {
+            Claim[] claims = [new("userId", user.Id.ToString())];
+
+            var signingCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.SecretKey)),
+                SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                signingCredentials: signingCredentials,
+                expires: DateTime.UtcNow.AddHours(_options.ExpitesHours));
+
+            var tokenValue = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return tokenValue;
+        }
+    }
+}
