2b9047ca329d0b45d896e7cc275e54d81662fdcc
airat <airat.hb@mail.ru>
2024-05-30 21:22:49 +0300

    change login

---
+++ User.Application/Services/UserService.cs
@@ -0,0 +1,53 @@
+﻿using UserAuthentication.Domain.Abstaraction;
+using UserAuthentication.Domain.Abstractions;
+using UserAuthentication.Domain.Models;
+
+namespace UserAuthentication.Application.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IPasswordHasher _passwordHasher;
+        private readonly IJwtProvider _jwtProvider;
+
+        public UserService(IUserRepository userRepository,
+                           IPasswordHasher passwordHasher,
+                           IJwtProvider jwtProvider)
+        {
+            _userRepository = userRepository;
+            _passwordHasher = passwordHasher;
+            _jwtProvider = jwtProvider;
+        }
+
+        public async Task Register(string username, string email, string password)
+        {
+            var hashedPassword = _passwordHasher.Generate(password);
+
+            var user = User.Create(Guid.NewGuid(), username, email, hashedPassword);
+
+            await _userRepository.Add(user);
+        }
+
+        public async Task<List<User>> GetAllUser()
+        {
+            return await _userRepository.Get();
+        }
+
+        public async Task<string> Login(string email, string password)
+        {
+            var user  = await _userRepository.GetByEmail(email);
+
+            var result = _passwordHasher.Verify(password, user.PasswordHash);
+
+            if (result == false)
+            {
+                return "пошел нахуй";
+                throw new Exception("Failed to login");
+            }
+
+            var token = _jwtProvider.GenerateToken(user);
+
+            return token;
+        }
+    }
+}
