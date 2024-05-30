beff48fd66e8133cbf736c51e5cce2ec63a5762b
airat <airat.hb@mail.ru>
2024-05-30 16:19:57 +0300

    new Migrathions

--- TaskManager.DataAccess/Migrations/20240530131558_AddUserAndFk.Designer.cs
+++ TaskManager.DataAccess/Migrations/20240530131558_AddUserAndFk.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TaskManager.DataAccess;
@@ -11,9 +12,11 @@ using TaskManager.DataAccess;
 namespace TaskManager.DataAccess.Migrations
 {
     [DbContext(typeof(TaskManagerDbContext))]
-    partial class TaskManagerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240530131558_AddUserAndFk")]
+    partial class AddUserAndFk
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
