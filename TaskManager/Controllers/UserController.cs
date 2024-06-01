d3847e4063b0606d821658d38a3d83c23cf9787c
airat <airat.hb@mail.ru>
2024-06-01 21:30:25 +0300

    add test tmp controller

--- TaskManager/Controllers/UserController.cs
+++ TaskManager/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using TaskManager.Contracts.Task;
 using TaskManager.Contracts.User;
 using UserAuthentication.Domain.Abstractions;
@@ -10,34 +11,58 @@ namespace TaskManager.Controllers
     public class UserController : Controller
     {
         private readonly IUserService _usersService;
+        private readonly ILogger<TaskController> _logger;
 
-        public UserController(IUserService usersService)
+        public UserController(IUserService usersService, ILogger<TaskController> logger)
         {
             _usersService = usersService;
+            _logger = logger;
         }
 
         [HttpPost]
         [Route("regist")]
-        public async Task Regist([FromBody] LoginUser loginUser)
+        public async Task<ActionResult> Regist([FromBody] RegisterUserRequest registerUser)
         {
-            await _usersService.Register(loginUser.name, loginUser.email, loginUser.paswword);
+            try
+            {
+                _logger.LogInformation("StartRegist");
+                await _usersService.Register(registerUser.name, registerUser.email, registerUser.paswword);
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return Ok();
         }
 
         [HttpPost]
         [Route("login")]
-        public async Task<string> Login([FromBody] LoginUserResponse loginUser)
+        public async Task<string> Login([FromBody] LoginUserRequest loginUser)
         {
+            var token = await _usersService.Login(loginUser.email, loginUser.paswword);
+
+            ControllerContext.HttpContext.Response.Cookies.Append("test", token);
+
             return await _usersService.Login(loginUser.email, loginUser.paswword);
         }
 
+
         [HttpGet]
         [Route("get")]
-        public async Task<ActionResult<List<LoginUser>>> Get()
+        public async Task<ActionResult<List<RegisterUserRequest>>> Get()
         {
             var users = await _usersService.GetAllUser();
             var response = users
-                .Select(b => new LoginUser(b.Name, b.Email, b.PasswordHash));
+                .Select(b => new RegisterUserRequest(b.Name, b.Email, b.PasswordHash));
             return Ok(response);
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("a")]
+        public async Task<ActionResult<string>> Asd()
+        {
+            return Ok("хорошо");
         }
     }
 }
